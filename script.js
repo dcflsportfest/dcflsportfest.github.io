d66432846d39bc8dcaa07654fb3b3444856b9b93
dcflsportfest <taylansahin324@gmail.com>
2026-03-05 18:01:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const eventDate = new Date("May 12, 2026 09:00:00").getTime();
+
+setInterval(function () {
+  const now = new Date().getTime();
+  const distance = eventDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + " Gün " + hours + " Saat " + minutes + " Dakika " + seconds + " Saniye";
+
+}, 1000);
